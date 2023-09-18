9070a482a629d26f5ea50577202ffe72e6555ace
Moh Oktavi Aziz Nugraha <moktavizzen@gmail.com>
2023-09-18 12:46:47 +0700

    add user in User.php and DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,12 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
+
+        User::factory()->create([
+            'email' => 'inspektorat@gresikkab.go.id',
+            'name' => 'Admin Inspektorat',
+            'password' => bcrypt('Admininspektorat1')
+        ]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
