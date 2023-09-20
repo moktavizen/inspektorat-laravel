b13040af6c6ecaaf4cb871b573d5e9f08b186718
Moh Oktavi Aziz Nugraha <moktavizzen@gmail.com>
2023-09-21 06:54:04 +0700

    add favicon to users pages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,9 +3,12 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta name="application-name" content="{{ config('app.name') }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Inspektorat | Gresik</title>
+    <link rel="icon" type="image/x-icon" href="{{ asset('/images/favicon.ico') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
